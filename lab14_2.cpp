e1dd39f4e21a31da7ca9394bb302105aec1d8be1
PiKr-Salmon <piyawat_kr@cmu.ac.th>
2024-01-11 19:25:58 +0700

    lab14_2 complete

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -19,3 +19,37 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+
+void inputMatrix(double u[][N]){
+    for(int ir = 0; ir < N; ir++){
+        cout << "Row " << ir+1 << ": ";
+        for(int in = 0; in < N ; in++){
+            double inp;
+            cin >> inp;
+            u[ir][in] = inp;
+        }
+    }
+}
+
+void findLocalMax(const double A[][N], bool B[][N]){
+    for(int row = 0; row < N ;row++){
+        for(int colum = 0; colum < N ; colum++){
+		    if(A[row][colum] >= A[row-1][colum] && A[row][colum] >= A[row+1][colum] && A[row][colum] >= A[row][colum-1] && A[row][colum] >= A[row][colum+1]){
+		            B[row][colum] = 1.0 ;
+		    }else B[row][colum] = 0.0;
+		    if(row == 0 || row == N-1)
+                B[row][colum] = 0.0 ;
+            else if (colum == 0 || colum == N-1)
+                B[row][colum] = 0.0 ;
+		}
+	}
+}
+
+void showMatrix(const bool u[][N]){
+   for(int row = 0; row < N ;row++)
+        for(int colum = 0; colum < N ; colum++){
+            if(colum != N-1){
+            cout << u[row][colum] << " " ;
+            }else cout << u[row][colum] << endl;
+        }
+}
